b662d3cc7655fec66137c5f73ac8f835fe647fed
Marco <marcomotolese777@gmail.com>
2026-04-13 20:18:04 +0200

    aggiustato deploy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,9 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->create()
-    ->tap(function (Application $app): void {
-        if (isset($_ENV['APP_STORAGE'])) {
-            $app->useStoragePath($_ENV['APP_STORAGE']);
-        }
-    });
+    ->create();
+
+if (isset($_ENV['APP_STORAGE'])) {
+    $app->useStoragePath($_ENV['APP_STORAGE']);
+}
+
+return $app;
