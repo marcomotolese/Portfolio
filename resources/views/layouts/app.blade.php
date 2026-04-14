ff3d7fe3d8a91e770999d0c29ebbdb4c301cbb49
Marco <marcomotolese777@gmail.com>
2026-04-14 04:38:15 +0200

    aggiunto favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('title', config('app.name', 'Portfolio'))</title>
+        <link rel="icon" type="image/jpeg" href="{{ asset('images/logo.jpg') }}">
+        <link rel="apple-touch-icon" href="{{ asset('images/logo.jpg') }}">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700" rel="stylesheet" />
